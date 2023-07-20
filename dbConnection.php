939ceaa406f9ac21b57489d5fd00ab15bbfa280a
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-07-20 09:34:43 +0100

    bug: bug fixed

--- dbConnection.php
+++ dbConnection.php
@@ -4,8 +4,8 @@ use Dotenv\Dotenv;
 
 // require resource: Connection Object
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'dbSource/dbConn.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'dbSource/dbController.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'dbSource/dbTable.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'dbSource/dbTableOp.php';
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -13,8 +13,7 @@ $dotenv->load();
 /******* Create/Drop Database ******/
 function dbConnection(string $databaseName): bool
 {
-    $conn = new DbConnection($_ENV["DATABASE_HOSTNAME"], $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"]);
-    $conn = $conn->getConnection();
+    $conn = new DbTable($_ENV["DATABASE_HOSTNAME"], $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"]);
 
     if (!$conn instanceof mysqli) {
         throw new Exception('Connection failed.');
@@ -32,23 +31,18 @@ function dbConnection(string $databaseName): bool
 /******* Create/Drop/Truncate/Alter Table ******/
 function tableConnection(string $databaseName): DbTable
 {
-    $conn = new DbConnection($_ENV["DATABASE_HOSTNAME"], $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"], $databaseName);
-    $conn = $conn->getConnection();
-    $conn = new DbTable($conn);
+    $conn = new DbTable($_ENV["DATABASE_HOSTNAME"], $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"], $databaseName);
     return $conn;
 }
 
 /******* Table Read and Write Operations ******/
 function tableOpConnection(string $databaseName): DbTableOps
 {
-    $connection = new DbConnection($_ENV["DATABASE_HOSTNAME"], $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"], $databaseName);
-    $conn = $connection->getConnection();
-    $conn = new DbTableOps($conn);
+    $conn = new DbTableOps($_ENV["DATABASE_HOSTNAME"], $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"], $databaseName);
     return $conn;
 }
 
-
-/** *******************************************Create Databases***************************************** */
+/** ******************************************* Create or Drop Database ***************************************** */
 $databaseName = [];
 foreach ($databaseName as $database) {
     // print_r(dbConnection($database));
--- dbSource/dbTableOp.php
+++ dbSource/dbTableOp.php
@@ -1,136 +1,5 @@
 <?php
 
-/**
- * Table Creation Class.
- * Requires Resource
- * to Create Tables in Database.
- */
-class DbTable
-{
-    private ?mysqli $conn;
-
-    /**
-     * Constructor,
-     * sources resource: connection object
-     */
-    public function __construct(?mysqli $conn)
-    {
-        $this->conn = $conn;
-    }
-
-    /**
-     * @param string $tableName = "Name of table to be created in database"
-     * @param string $fieldNames = "fieldName dataType NULL/NOT NULL ?PRIMARY KEY ?AUTO_INCREMENT, fieldName dataType NULL/NOT NULL ?DEFAULT"
-     * @return bool True if the table was created successfully, false otherwise
-     */
-    public function createTable(string $tableName, string $fieldNames): bool
-    {
-        if (!$this->conn instanceof mysqli) {
-            throw new Exception("No database connection available.");
-        }
-
-        $sql_query = "CREATE TABLE IF NOT EXISTS $tableName ($fieldNames)";
-        $result = $this->conn->query($sql_query);
-
-        if ($result !== true) {
-            throw new Exception("Error! Table Creation Failed: " . $this->conn->error);
-        }
-
-        // $this->conn->close(); // Close Connection Object
-        return $result === true;
-    }
-
-    /**
-     * @param string $tableName Name of the table to be altered in the database
-     * @param string $alterStatement Statement to modify the table structure
-     * @return bool True if the table was altered successfully, false otherwise
-     */
-    public function alterTable(string $tableName, string $alterStatement): bool
-    {
-        if (!$this->conn instanceof mysqli) {
-            throw new Exception("No database connection available.");
-        }
-
-        $sql_query = "ALTER TABLE $tableName $alterStatement";
-        $result = $this->conn->query($sql_query);
-
-        if ($result !== true) {
-            throw new Exception("Error! Process Failed: " . $this->conn->error);
-        }
-
-        // $this->conn->close(); // Close Connection Object
-        return $result === true;
-    }
-
-    /**
-     * @param string $tableName Name of the table to be truncated in the database
-     * @return bool True if the table was truncated successfully, false otherwise
-     */
-    public function truncateTable(string $tableName): bool
-    {
-        if (!$this->conn instanceof mysqli) {
-            throw new Exception("No database connection available.");
-        }
-
-        $sql_query = "TRUNCATE TABLE $tableName";
-        $result = $this->conn->query($sql_query);
-
-        if ($result !== true) {
-            throw new Exception("Error! Process Failed: " . $this->conn->error);
-        }
-
-        // $this->conn->close(); // Close Connection Object
-        return $result === true;
-    }
-
-    /**
-     * @param string $tableName Name of the table to be dropped in the database
-     * @return bool True if the table was dropped successfully, false otherwise
-     */
-    public function dropTable(string $tableName): bool
-    {
-        if (!$this->conn instanceof mysqli) {
-            throw new Exception("No database connection available.");
-        }
-
-        $sql_query = "DROP TABLE $tableName";
-        $result = $this->conn->query($sql_query);
-
-        if ($result !== true) {
-            throw new Exception("Error! Process Failed: " . $this->conn->error);
-        }
-
-        // $this->conn->close(); // Close Connection Object
-        return $result === true;
-    }
-
-    /**
-     * Retrieves the names of tables in a database as an array containing values only.
-     * @param string $databaseName Name of the database
-     * @return array Array containing the names of tables
-     */
-    public function retrieveTableNames(string $databaseName): array
-    {
-        if (!$this->conn instanceof mysqli) {
-            throw new Exception("No database connection available.");
-        }
-
-        $sql_query = "SHOW TABLES FROM $databaseName";
-        $result = $this->conn->query($sql_query);
-
-        if ($result) {
-            $tableNames = [];
-            while ($row = $result->fetch_row()) {
-                $tableNames[] = $row[0];
-            }
-            $result->close(); // Close Result Object
-            return $tableNames;
-        }
-        // $this->conn->close(); // Close connection
-        return [];
-    }
-}
-
 /*
  * Table Read and Write Class.
  * Requires Resource
@@ -138,6 +7,8 @@ class DbTable
  */
 class DbTableOps
 {
+    use DbConn;
+
     private ?mysqli $conn;
 
     /**
@@ -146,7 +17,7 @@ class DbTableOps
      */
     public function __construct(?mysqli $conn)
     {
-        $this->conn = $conn;
+        $this->conn = $this->dbConn($_ENV["DATABASE_HOSTNAME"], $_ENV["DATABASE_USERNAME"], $_ENV["DATABASE_PASSWORD"]);
     }
 
     private function getBindParamTypes(array $values): string
