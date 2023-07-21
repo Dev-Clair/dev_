76013fa0f43f3d54ce6f0f1a04d1e7c4341f7a9c
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-07-21 12:01:59 +0100

    fix: .gitignore

--- src/index.php
+++ src/index.php
@@ -5,6 +5,5 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/db/dbResource.php';
 
-$databaseName = "employee";
+$databaseName = "";
 $conn = tableConnection($databaseName);
-var_dump($conn);
